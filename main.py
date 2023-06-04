2e6f3076badcce6ad548d00729a69d27465ddf42
MehmetZekiAKTAS <133994388+MehmetZekiAKTAS@users.noreply.github.com>
2023-06-05 01:27:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,27 @@ print()
 print("Insan 2:")
 print(insan2)
 print()
+# Issiz sınıfı için örnek nesneler oluşturulması
+issiz1 = Issiz("45621537925 "Ali", "Kara", 35, "Erkek", "Türk", 0)
+issiz2 = Issiz("17952684631", "Sudenaz", "Gürcan", 40, "Kadın", "Türk", 3)
+issiz3 = Issiz("31695265781", "Mehmet", "Beyaz", 50, "Erkek", "Türk", 6)
+
+# Issiz sınıfı için örnek nesnelerin bilgilerinin yazdırılması
+print("Issiz 1:")
+print(issiz1)
+issiz1.statu_bul()
+print(f"Statü: {issiz1.get_statu()}")
+print()
+
+print("Issiz 2:")
+print(issiz2)
+issiz2.statu_bul()
+print(f"Statü: {issiz2.get_statu()}")
+print()
+
+print("Issiz 3:")
+print(issiz3)
+issiz3.statu_bul()
+print(f"Statü: {issiz3.get_statu()}")
+print()
+
