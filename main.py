71a7a1504dc6ef51568290ebd6879f42cb503b4f
MehmetZekiAKTAS <133994388+MehmetZekiAKTAS@users.noreply.github.com>
2023-06-05 01:26:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from Calisan import Calisan
 from MaviYaka import MaviYaka
 from BeyazYaka import BeyazYaka
 
+# Insan sınıfı için örnek nesneler oluşturulması
+insan1 = Insan("64615326223", "Renas", "Aktaş", 30, "Erkek", "Türk")
+insan2 = Insan("16006080252", "Erva", "Sarıkan", 25, "Kadın", "Türk")
+
+# Insan sınıfı için örnek nesnelerin bilgilerinin yazdırılması
+print("Insan 1:")
+print(insan1)
+print()
+
+print("Insan 2:")
+print(insan2)
+print()
