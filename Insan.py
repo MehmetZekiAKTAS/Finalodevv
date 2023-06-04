2824212361838f6cfa54a988312fabec15ce9934
MehmetZekiAKTAS <133994388+MehmetZekiAKTAS@users.noreply.github.com>
2023-06-05 01:14:05 +0300

    Update Insan.py

---
+++ Insan.py
@@ -0,0 +1,8 @@
+class Insan:
+    def __init__(self, tc_no, ad, soyad, yaş, cinsiyet, uyruk):
+        self.__tc_no = tc_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__yaş = yaş
+        self.__cinsiyet = cinsiyet
+        self.__uyruk = uyruk
